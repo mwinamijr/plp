bc60d20a0b5832a88cae3021582b78f24f924b87
mwinamijr <mwinamijr@gmail.com>
2024-02-13 11:46:46 +0300

    created and added contents in user_input file

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("Please enter your name: ")
+
+age = input("Please enter your age: ")
+
+location = input("Where do you live: ")
+
+print("Hello {}, you are {} years old and live in {}".format(name, age, location))
